ca8659fff04d5cb6995fd25d3c9a4e64dc62d43e
Karolinka <Karolina.Herlender@gmail.com>
2015-09-28 22:42:48 +0200

    lala

---
+++ static/js/diseasecontroller.js
@@ -0,0 +1,12 @@
+var diseaseApp = angular.module('diseaseApp', []);
+
+diseaseApp.controller('DiseaseListCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
